41f11f2335f8fbf9686c2e10f9490d97bd78e92f
Vadikgit <56280697+Vadikgit@users.noreply.github.com>
2025-01-16 13:42:32 +0300

    Update run.py

--- server/run.py
+++ server/run.py
@@ -14,5 +14,5 @@ app, db = create_app()
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 if __name__ == '__main__':
-    print(111111111)
+    print(11111111)
     app.run(host='0.0.0.0', debug=True)
