f66746e21ffdcc09ff44150e5a179a221821dd5c
Vadikgit <56280697+Vadikgit@users.noreply.github.com>
2025-01-16 15:42:26 +0300

    Update makefriends.vue

--- client/src/components/makefriends.vue
+++ client/src/components/makefriends.vue
@@ -8,7 +8,7 @@
           type="button"
           class="btn btn-success btn-sm"
           @click="toggleAddUserModal">
-          Add User
+          Append User
         </button>
         <br><br>
         <button
